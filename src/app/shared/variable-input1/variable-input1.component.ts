66723f091f25ac7a5e2ce4426343e9871ce1e1b9
yuan1701 <1349281070@qq.com>
2021-05-13 01:47:33 +0800

    feat: 完善输入框，新建一个demo实现rotate的实现效果

--- src/app/shared/variable-input1/variable-input1.component.ts
+++ src/app/shared/variable-input1/variable-input1.component.ts
@@ -17,40 +17,63 @@ export class VariableInput1Component implements OnInit {
   ngOnInit(): void {}
 
   // 需要控制输入参数
-  onKeyUp(event: KeyboardEvent): void {
-    console.log(event.key);
+  onKeydown(event: KeyboardEvent): void {
+    // 1.不支持换行
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      return;
+    }
+    // 2.调出弹框
     if (event.key === '$') {
-      console.log(this.inputValue);
-    } else {
-      const reg = new RegExp('^[a-zA-Z0-9]{1,1}$');
-      if (reg.test(event.key)) {
-        console.log(event.key);
-      }
+      alert(1);
+      event.preventDefault();
+      return;
     }
-    // 记录光标的位置
+    // 3.记录光标的位置
     const selection = getSelection();
-    this.lastRange = selection && selection.getRangeAt(0);
-    console.log(this.lastRange);
+    this.lastRange = selection && (selection.getRangeAt(0) as Range);
   }
 
-  onBlur(event: any) {
-    this.inputValue = event.target.innerHTML;
+  onBlur(event: FocusEvent) {
+    // this.inputValue = event.target.innerHTML;
     // 记录光标的位置
     const selection = getSelection();
     this.lastRange = selection && selection.getRangeAt(0);
   }
 
-  addVar() {
-    console.log(this.lastRange);
-
-    if (0 || this.lastRange) {
-      // 2.光标位置插入变量？插入位置不对
-      var span = document.createElement('span');
-      span.innerHTML = '${name}';
-      span.className = 'tag-item';
-      span.setAttribute('contenteditable', 'false');
-      this.lastRange.insertNode(span);
-      this.lastRange.setStart(span, 1);
+  // 粘贴板
+  onPaste(e: ClipboardEvent): void {
+    e.preventDefault();
+    let paste = e.clipboardData?.getData('text');
+    const selection = window.getSelection();
+    if (!selection?.rangeCount) {
+      return;
     }
+    selection.deleteFromDocument();
+    paste && selection.getRangeAt(0).insertNode(document.createTextNode(paste));
+  }
+
+  addVar(name: string): void {
+    console.log(this.lastRange);
+    // 1.获取选定的对象,清楚选中的光标
+    const selection = getSelection();
+    if (this.lastRange) {
+      selection?.removeAllRanges();
+      selection?.addRange(this.lastRange);
+    }
+
+    // 2.变量位置不能添加变量
+    const parentNode = this.lastRange.startContainer.parentNode;
+    if (parentNode.className === 'tag-item') {
+      return;
+    }
+
+    // 3.光标位置插入变量
+    var span = document.createElement('span');
+    span.innerHTML = '${' + name + '}';
+    span.className = 'tag-item';
+    span.setAttribute('contenteditable', 'false');
+    this.lastRange.insertNode(span);
+    this.lastRange.setStart(span, 1);
   }
 }
